f22cf69ac98acb3699d0e68f5d5d765285d31707
owaisk23 <owais1566@gmail.com>
2025-04-18 20:50:06 +0500

    listView and Singlechild

--- MobAppDevelopment/Flutter/firstproj/lib/firstScreen.dart
+++ MobAppDevelopment/Flutter/firstproj/lib/firstScreen.dart
@@ -18,40 +18,149 @@ class _FirstScreenState extends State<FirstScreen> {
         foregroundColor: Colors.white,
         centerTitle: true,
       ),
-      body: Center(
-        child: Column(
+      body: Padding(
+          // padding: const EdgeInsets.all(10),
+          // padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+          padding: const EdgeInsets.only(top: 40, left: 20, right: 20),
+          child: ListView(
+            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Container(
               height: 100,
               width: 100,
               color: Colors.red,
-            ),
-            SizedBox(height: 10,),
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.yellow,
-            ),
-            SizedBox(height: 10,),
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.blue,
+              child: Icon(Icons.home, color: Colors.white, size: 50,),
             ),
             SizedBox(height: 10,),
             Container(
               height: 100,
               width: 100,
               color: Colors.pink,
+              child: Icon(Icons.wheelchair_pickup, color: Colors.white, size: 50,),
             ),
             SizedBox(height: 10,),
             Container(
               height: 100,
               width: 100,
-              color: Colors.green,
-            )
+              color: Colors.blue,
+              child: Icon(Icons.phone, color: Colors.white, size: 50,),
+            ),
+            SizedBox(height: 10,),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.teal,
+              child: Icon(Icons.tab, color: Colors.white, size: 50,),
+            ),
+            SizedBox(height: 10,),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.deepPurpleAccent,
+              child: Icon(Icons.h_mobiledata, color: Colors.white, size: 50,),
+            ),
+            SizedBox(height: 10,),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.deepOrange,
+              child: Icon(Icons.bus_alert, color: Colors.white, size: 50,),
+            ),
+            SizedBox(height: 10,),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.blueGrey,
+              child: Icon(Icons.car_crash, color: Colors.white, size: 50,),
+            ),
+            SizedBox(height: 10,),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.greenAccent,
+              child: Icon(Icons.bike_scooter, color: Colors.white, size: 50,),
+            ),
+            SizedBox(height: 10,),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.brown,
+              child: Icon(Icons.ballot, color: Colors.white, size: 50,),
+            ),
         ],)
+        
       ),
+      // body: Center(
+      //   child: SingleChildScrollView(
+      //     scrollDirection: Axis.vertical,
+      //     child: Column(
+      //       // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //     children: [
+      //       Container(
+      //         height: 100,
+      //         width: 100,
+      //         color: Colors.red,
+      //         child: Icon(Icons.home, color: Colors.white, size: 50,),
+      //       ),
+      //       SizedBox(height: 10,),
+      //       Container(
+      //         height: 100,
+      //         width: 100,
+      //         color: Colors.pink,
+      //         child: Icon(Icons.wheelchair_pickup, color: Colors.white, size: 50,),
+      //       ),
+      //       SizedBox(height: 10,),
+      //       Container(
+      //         height: 100,
+      //         width: 100,
+      //         color: Colors.blue,
+      //         child: Icon(Icons.phone, color: Colors.white, size: 50,),
+      //       ),
+      //       SizedBox(height: 10,),
+      //       Container(
+      //         height: 100,
+      //         width: 100,
+      //         color: Colors.teal,
+      //         child: Icon(Icons.tab, color: Colors.white, size: 50,),
+      //       ),
+      //       // SizedBox(height: 10,),
+      //       // Container(
+      //       //   height: 100,
+      //       //   width: 100,
+      //       //   color: Colors.deepPurpleAccent,
+      //       //   child: Icon(Icons.h_mobiledata, color: Colors.white, size: 50,),
+      //       // ),
+      //       // SizedBox(height: 10,),
+      //       // Container(
+      //       //   height: 100,
+      //       //   width: 100,
+      //       //   color: Colors.deepOrange,
+      //       //   child: Icon(Icons.bus_alert, color: Colors.white, size: 50,),
+      //       // ),
+      //       // SizedBox(height: 10,),
+      //       // Container(
+      //       //   height: 100,
+      //       //   width: 100,
+      //       //   color: Colors.blueGrey,
+      //       //   child: Icon(Icons.car_crash, color: Colors.white, size: 50,),
+      //       // ),
+      //       // SizedBox(height: 10,),
+      //       // Container(
+      //       //   height: 100,
+      //       //   width: 100,
+      //       //   color: Colors.greenAccent,
+      //       //   child: Icon(Icons.bike_scooter, color: Colors.white, size: 50,),
+      //       // ),
+      //       // SizedBox(height: 10,),
+      //       // Container(
+      //       //   height: 100,
+      //       //   width: 100,
+      //       //   color: Colors.brown,
+      //       //   child: Icon(Icons.ballot, color: Colors.white, size: 50,),
+      //       // ),
+      //   ],)
+      //   ),
+      // ),
     );
   }
 }
