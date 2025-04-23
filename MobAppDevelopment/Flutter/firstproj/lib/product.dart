b8006c4295f8fa7f6597db1fadeb6590493df0a1
owaisk23 <owais1566@gmail.com>
2025-04-23 20:19:17 +0500

    added buttons and text profile card with page navigation

--- MobAppDevelopment/Flutter/firstproj/lib/product.dart
+++ MobAppDevelopment/Flutter/firstproj/lib/product.dart
@@ -1,3 +1,4 @@
+import 'package:firstproj/firstScreen.dart';
 import 'package:flutter/material.dart';
 
 class Product extends StatefulWidget {
@@ -26,7 +27,7 @@ class _ProductState extends State<Product> {
             border: Border.all(color: Colors.black, width: 2),
           ),
          child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10), 
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10), 
           child: Column(
             children: [
               Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQ_H9lmgYtSdi8bF9KV_jcwFNcnqT34rRvfw&s',
@@ -34,6 +35,47 @@ class _ProductState extends State<Product> {
                width: double.infinity,
                fit: BoxFit.cover,
               ),
+              SizedBox(height: 15,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text("Lenovo Thinkpad V14", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: const Color.fromARGB(255, 33, 33, 33)),),
+                  Text("\$ 500", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: const Color.fromARGB(255, 33, 33, 33)),),
+                ],
+              ),
+              SizedBox(height: 15,),
+              Text("Everyday used Everyday used machine for office and personal task Leveno Core i7 14Generation Everyday used machine for office and personal task Leveno Core i7 14Generation", style: TextStyle(fontSize: 12, color: const Color.fromARGB(255, 95, 95, 95)),),
+              SizedBox(height: 15,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {}, 
+                    child: Text("Add to Cart"),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 10,),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => FirstScreen()));
+                    }, 
+                    child: Text("Buy Now"),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                ],
+              ),  
             ],
           ), 
         ),
