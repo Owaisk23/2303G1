51782f110c427ebfe49ca07155a6e71c7ada64ab
owaisk23 <owais1566@gmail.com>
2025-05-23 20:21:32 +0500

    image crud in firebase

---
+++ MobAppDevelopment/Flutter/firebase_crud/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crud/addproduct.dart';
+import 'package:firebase_crud/firebase_options.dart';
+import 'package:flutter/material.dart';
+
+void main()async {
+
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
+   runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Addproduct()
+    ),
+  );
+}
--- MobAppDevelopment/Flutter/firstproj/lib/main.dart
+++ MobAppDevelopment/Flutter/firstproj/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:device_preview/device_preview.dart';
 // import 'package:firstproj/ApiProduct.dart';
 import 'package:firstproj/BillCalculator.dart';
+
 // import 'package:firstproj/SongApp.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
